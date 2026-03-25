c3caac0f95471503206c530b416d250fa5861a1b
criticalart <heels.rower.7z@icloud.com>
2026-03-24 18:40:28 -0700

    th-thanks awww

--- monitor-swap.sh
+++ monitor-swap.sh
@@ -3,6 +3,6 @@
 clear && fastfetch && echo $'\n' && fortune
 cp ~/.config/hypr/monitor.mode ~/.config/hypr/monitors.conf
 sleep 3
-swww img -o "DP-1" --transition-fps 144 --transition-type right --transition-duration 2 ~/.config/calos/current/background
+awww img -o "DP-1" --transition-fps 144 --transition-type right --transition-duration 2 ~/.config/calos/current/background
 sleep 1
 hyprctl dispatch movetoworkspace 1
--- tv-mode.sh
+++ tv-mode.sh
@@ -6,7 +6,7 @@ if pgrep -x waybar >/dev/null; then
 fi
 cp ~/.config/hypr/tv.mode ~/.config/hypr/monitors.conf
 sleep 3
-swww img -o "HDMI-A-1" --transition-fps 60 --transition-type left --transition-duration 2 ~/.config/calos/current/background
+awww img -o "HDMI-A-1" --transition-fps 60 --transition-type left --transition-duration 2 ~/.config/calos/current/background
 sleep 1
 hyprctl dispatch movetoworkspace 6
 sleep 1
