12403aea26b0b555add159698dc5cdc25c0b462e
criticalart <heels.rower.7z@icloud.com>
2026-05-16 16:18:04 -0700

    updated to lua

--- bigpic.sh
+++ bigpic.sh
@@ -4,7 +4,7 @@ echo
 notify-send "Launching Steam Big Picture Mode"
 steam steam://open/bigpicture
 sleep 1
-hyprctl dispatch workspace 10
+hyprctl dispatch 'hl.dsp.focus({ workspace = "10" })'
 sleep 5
-hyprctl dispatch focuswindow title:Steam Big Picture Mode && hyprctl dispatch fullscreen
+hyprctl dispatch 'hl.dsp.window.fullscreen({ fullscreen, set, window = "title:Steam Big Picture Mode" })'
 exit
